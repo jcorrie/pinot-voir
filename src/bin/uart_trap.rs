d713b505c9ed19b7f8c74a11f6d91f4e247361c5
Jonny Corrie <jonnycorrie@gmail.com>
2026-08-10 16:13:54 +0100

    update crgo toml

--- src/bin/uart_trap.rs
+++ src/bin/uart_trap.rs
@@ -72,7 +72,6 @@ async fn main(spawner: Spawner) {
     write!(url_string, "{base_url_string}/api/utils/trap_trigger/1")
         .expect("Failed to write server url string.");
     loop {
-        // info!(&url_string.as_str());
         // if trap_pin_1.is_low() {
         let mut request = match http_client.request(Method::GET, &url_string.as_str()).await {
             Ok(req) => req,
